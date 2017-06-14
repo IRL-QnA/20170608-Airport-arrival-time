4888ec63fd8602f0800da14386d181885660a807
Hepheir <hepheir@gmail.com>
2017-06-14 22:11:22 +0900

    .
    (cherry picked from commit 08a6451ce954f2715edfdc054e66c69cbbf11a9f)

--- 김주은/2. 공항 도착시간/index.py
+++ 김주은/2. 공항 도착시간/index.py
@@ -1,9 +1,9 @@
-def parse_time(a):
-    b = a.split(':')
-    return [int(b[0]), int(b[1])]
+def parse_time(string):
+    a = string.split(':')
+    return [int(a[0]), int(a[1])]
 
-def read_line(a):
-    return a.readline().replace('\n', '')
+def read_line(file):
+    return file.readline().replace('\n', '')
 
 def digit(a):
     if a < 10:
@@ -18,7 +18,7 @@ foutput = open('output.txt', 'w')
 line = read_line(finput)
 foutput.write(line)
 
-flight_time = parse_time(line.split('/')[1])
+flight_time = parse_time(line.split(' / ')[1])
 
 while True:
     line = read_line(finput)
@@ -28,7 +28,10 @@ while True:
 
     take_off_time = parse_time(line)
 
-    arrival_time = [take_off_time[0] + flight_time[0], take_off_time[1] + flight_time[1]]
+    arrival_time = [
+        take_off_time[0] + flight_time[0],
+        take_off_time[1] + flight_time[1]
+    ]
 
     if arrival_time[1] > 59:
         arrival_time[0] += 1
